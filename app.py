2b569e835bad587387f05e55b85643b85465c41b
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2020-11-13 12:31:05 -0500

    ⚡ new feature added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from guizero import App, Text, PushButton
 app = App(title="guizero")
 
 intro = Text(app, text="Have a go with guizero and see what you can create.")
+picture = Picture(app, image="test.gif")
 ok = PushButton(app, text="Ok")
 
 app.display()
